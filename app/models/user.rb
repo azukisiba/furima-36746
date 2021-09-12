e5929623a7c33e65adf295d425dfa6ef52c74c99
azukisiba <azukisiba398@gmail.com>
2021-09-12 19:40:20 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :kanji_name_first, presence: true
+  validates :kanji_name_last, presence: true
+  validates :kana_name_first, presence: true
+  validates :kana_name_last, presence: true
+  validates :birth_date, presence: true
 end
