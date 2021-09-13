50e35531198a2a9a5184f4d3d90458f8736406d7
azukisiba <azukisiba398@gmail.com>
2021-09-13 14:13:17 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :kanji_name_first, presence: true
-  validates :kanji_name_last, presence: true
-  validates :kana_name_first, presence: true
-  validates :kana_name_last, presence: true
   validates :birth_date, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :kanji_name_first
+    validates :kanji_name_last
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :kana_name_first
+    validates :kana_name_last
+  end
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
