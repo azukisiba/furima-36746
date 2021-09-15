2221d8a02c3daebb1d64f2d0d4813eec2f99a933
azukisiba <azukisiba398@gmail.com>
2021-09-15 11:37:41 +0900

    ActiveHash関連の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :load
@@ -8,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :delivery
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :states_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :load_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
