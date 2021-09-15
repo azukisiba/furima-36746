b1872475d6ffe48ab84caf8398cafe67ef17fc0f
azukisiba <azukisiba398@gmail.com>
2021-09-15 11:38:04 +0900

    出品時のエラー表示設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,19 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
   def items_params
     params.require(:item).permit(:title, :item_description, :category_id, :status_id,
