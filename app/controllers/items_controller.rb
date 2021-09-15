9eb14c5b85b13b686a5a06fee4d3f11cfd18c43b
azukisiba <azukisiba398@gmail.com>
2021-09-15 18:10:28 +0900

    rubocopによる自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,8 +17,9 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def items_params
     params.require(:item).permit(:title, :item_description, :category_id, :status_id,
-      :load_id, :area_id, :delivery_id, :selling_price, :image).merge(user_id: current_user.id)
+                                 :load_id, :area_id, :delivery_id, :selling_price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,12 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :delivery
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :load_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :load_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true
   validates :title, presence: true
   validates :item_description, presence: true
