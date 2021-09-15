c2480f6b7c8fbda95c2b599c5700d2edc07455e4
azukisiba <azukisiba398@gmail.com>
2021-09-15 09:54:11 +0900

    ActiveHashの準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :load
+  belongs_to :area
+  belongs_to :delivery
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :states_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :load_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
