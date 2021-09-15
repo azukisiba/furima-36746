45fa141094dd451fb5d40af3e4b2ea7a0c59bba2
azukisiba <azukisiba398@gmail.com>
2021-09-15 10:07:38 +0900

    ストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
 
   private
   def items_params
-    params.require(:item).permit(:title, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :item_description, :category_id, :status_id,
+      :load_id, :area_id, :delivery_id, :selling_price, :image).merge(user_id: current_user.id)
   end
 end
