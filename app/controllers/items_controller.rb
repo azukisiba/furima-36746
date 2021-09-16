d5f17fec3a3159c518d96dfdadfa4c75210d10b8
azukisiba <azukisiba398@gmail.com>
2021-09-16 18:47:38 +0900

    editアクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to item_path(@item.id)
+    end
   end
 
   def update
