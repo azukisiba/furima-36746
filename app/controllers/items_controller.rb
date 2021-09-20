6fb37e417798a54f613eecd5b56af39f02974e7f
azukisiba <azukisiba398@gmail.com>
2021-09-20 16:37:33 +0900

    売却済みの自分の商品の編集ページに飛べないように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless @item.user == current_user
+    redirect_to root_path if @item.order.present? || @item.user != current_user
   end
 
   def update
