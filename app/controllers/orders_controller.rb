e5656eee23fd7a34d25fae8a62ce7f4ca97cf596
azukisiba <azukisiba398@gmail.com>
2021-09-20 16:34:09 +0900

    ログイン状態,購入済み,購入ページへの遷移→topページ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    redirect_to root_path if @item.user == current_user || @item.order.present?
     @credit = Credit.new
   end
 
