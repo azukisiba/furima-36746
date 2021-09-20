050c5ce45f8ee7e323fd0f14709c704884ed4472
azukisiba <azukisiba398@gmail.com>
2021-09-20 14:42:57 +0900

    factorybotの設定

---
+++ spec/factories/credits.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :credit do
+    postal_code { '123-4567' }
+    area_id { 1 }
+    city { '東京都' }
+    number { '1-1' }
+    building { '東京ハイツ' }
+    phone_num {'09011112222'}
+  end
+end
