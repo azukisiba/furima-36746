a99443b385f4f7b82b269fe2978956f6112b64e1
azukisiba <azukisiba398@gmail.com>
2021-09-20 15:35:48 +0900

    バリデーション　郵便番号、電話番号の制限追加

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -4,11 +4,11 @@ class Credit
   
   validates :user_id, presence: true
   validates :item_id, presence: true
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city, presence: true
   validates :number, presence: true
-  validates :phone_num, presence: true
+  validates :phone_num, presence: true, format: { with: /\A\d{10,11}\z/ }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
