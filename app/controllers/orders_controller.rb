52d12113fea706e6ffd5dc5f9c303e9c2ec33340
azukisiba <azukisiba398@gmail.com>
2021-09-20 12:20:38 +0900

    ストロングパラメーターの修正、createアクションの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,10 +6,18 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @credit = Credit.new(order_params)
+    if @credit.valid?
+      @credit.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
   def order_params
-    params.require(:credit).permit(:postal_code, :area_id, :city, :number, :building, :phone_num, :order).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:credit).permit(:postal_code, :area_id, :city, :number, :building, :phone_num, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
