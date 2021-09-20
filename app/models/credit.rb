d095faf5b0308ad6a6f4897886235e51d5471925
azukisiba <azukisiba398@gmail.com>
2021-09-20 12:19:41 +0900

    フォームオブジェクトの再設定

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,19 +1,19 @@
 class Credit
   include ActiveModel::Model
-  attr_accessor :user, :item, :postal_code, :area_id, :city, :number, :building, :phone_num, :order
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :number, :building, :phone_num, :order_id
   
-  validates :user, presence: true
-  validates :item, presence: true
+  validates :user_id, presence: true
+  validates :item_id, presence: true
   validates :postal_code, presence: true
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city, presence: true
   validates :number, presence: true
   validates :building, presence: true
   validates :phone_num, presence: true
-  validates :order, presence: true
+  validates :order_id, presence: true
 
   def save
-    Order.create(user: user, item: item)
-    Address.create(postal_code: postal_code, area_id: area_id, city: city, phone_num: phone_num, building: building)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, area_id: area_id, city: city, phone_num: phone_num, building: building, order_id: order_id)
   end
 end
