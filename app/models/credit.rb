9bb521ebb40a46eca588a0bda14d9a38215782d8
azukisiba <azukisiba398@gmail.com>
2021-09-22 12:12:24 +0900

    rubocopの自動修正

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -11,9 +11,8 @@ class Credit
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :phone_num, format: { with: /\A\d{10,11}\z/ }
   end
-  
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
