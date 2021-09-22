ce6b2143909f2ab0d373198499186a9d8ec85b52
azukisiba <azukisiba398@gmail.com>
2021-09-22 12:06:29 +0900

    creditのバリデーション　リファクタリング

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,14 +2,18 @@ class Credit
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :add_number, :building, :phone_num, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  with_options presence: true do
+    validates :user_id, presence: true
+    validates :item_id, presence: true
+    validates :city, presence: true
+    validates :add_number, presence: true
+    validates :token, presence: true
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :phone_num, format: { with: /\A\d{10,11}\z/ }
+  end
+  
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, presence: true
-  validates :add_number, presence: true
-  validates :phone_num, presence: true, format: { with: /\A\d{10,11}\z/ }
-  validates :token, presence: true
+
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
