5a6d25eaf1c981cfe40ddc8792ce1ef9fae70fa9
azukisiba <azukisiba398@gmail.com>
2021-09-23 11:31:11 +0900

    コメントモデル単体テストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :comment do
-    
+    text {'こんにちは'}
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Credit, type: :model do
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @comment = FactoryBot.build(:comment, user_id: user.id, item_id: item.id)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@comment).to be_valid
+      end
+    end
+    context '内容に問題がある場合' do
+      it 'textが空の場合には保存できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'userが紐づいていなければの場合には保存できない' do
+        @comment.user_id = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("User must exist")
+      end
+      it 'itemが紐づいていなければの場合には保存できない' do
+        @comment.item_id = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Item must exist")
+      end
+    end
+  end
 end
